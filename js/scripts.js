58895dc7209e7a5d96daf7b1ed6f7b4264e5d54d
Kate Hall <kwarrenhall@gmail.com>
2016-03-25 13:32:49 -0700

    finalized minimum viable product named index.html

--- js/scripts.js
+++ js/scripts.js
@@ -9,64 +9,68 @@ $(document).ready(function() {
 // summer
     // asia
   if (season === "summer" && region === "Asia" && budget === "$$$$" && grid === "offgrid" && kids === "no"){
-    result = "summer asia";
+    result = "summer Asia $$$$ offgrid no";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "summer" && region === "Asia" && budget === "$$$$" && grid === "offgrid" && kids === "yes") {
-      result = "summer europe";
+      result = "summer Asia $$$$ offgrid yes";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Asia" && budget === "$$$$" && grid === "resort" && kids === "no") {
-      result = "summer europe";
+      result = "summer Asia $$$$ resort no";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Asia" && budget === "$$$$" && grid === "resort" && kids === "yes") {
-      result = "summer europe";
+      result = "summer Asia $$$$ resort yes";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Asia" && budget === "$" && grid === "offgrid" && kids === "no") {
-      result = "summer europe";
+      result = "summer Asia $ off grid no";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Asia" && budget === "$" && grid === "offgrid" && kids === "yes") {
-      result = "summer europe";
+      result = "summer Asia $";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Asia" && budget === "$" && grid === "resort" && kids === "no") {
-      result = "summer europe";
+      result = "summer Asia $ no";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Asia" && budget === "$" && grid === "resort" && kids === "yes") {
-      result = "summer europe";
+      result = "summer Asia $";
       $("#recomendation").empty().text(result);
       $(".results").show();
   // europe
   } else if (season === "summer" && region === "Europe" && budget === "$$$$" && grid === "offgrid" && kids === "no") {
-      result = "summer europe";
+      result = "summer europe $$$$ no";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Europe" && budget === "$$$$" && grid === "offgrid" && kids === "yes") {
-      result = "summer europe";
+      result = "summer europe $$$$";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Europe" && budget === "$$$$" && grid === "resort" && kids === "no") {
-    result = "summer europe";
+    result = "summer europe $$$$ no";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "summer" && region === "Europe" && budget === "$$$$" && grid === "resort" && kids === "yes") {
-      result = "summer europe";
+      result = "summer europe $$$$";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Europe" && budget === "$" && grid === "offgrid" && kids === "no") {
-      result = "summer europe";
+      result = "summer europe $ no";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Europe" && budget === "$" && grid === "offgrid" && kids === "yes") {
-      result = "summer europe";
+      result = "summer europe $";
       $("#recomendation").empty().text(result);
       $(".results").show();
   } else if (season === "summer" && region === "Europe" && budget === "$" && grid === "resort" && kids === "no") {
-      result = "summer europe";
+      result = "summer europe $ no";
+      $("#recomendation").empty().text(result);
+      $(".results").show();
+  } else if (season === "summer" && region === "Europe" && budget === "$" && grid === "resort" && kids === "yes") {
+      result = "summer europe $";
       $("#recomendation").empty().text(result);
       $(".results").show();
 // Americas
@@ -105,101 +109,101 @@ $(document).ready(function() {
 // winter
 // winter asia
   } else if (season === "winter" && region === "Asia" && budget === "$$$$" && grid === "offgrid" && kids === "no") {
-    result = "summer europe";
+    result = "winter Asia $$$$ offgrid no";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Asia" && budget === "$$$$" && grid === "offgrid" && kids === "yes") {
-    result = "summer europe";
+    result = "winter Asia $$$$ offgrid yes";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Asia" && budget === "$$$$" && grid === "resort" && kids === "no") {
-    result = "summer europe";
+    result = "winter Asia $$$$ resort no";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Asia" && budget === "$$$$" && grid === "resort" && kids === "yes") {
-    result = "summer europe";
+    result = "winter Asia $$$$ resort yes";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Asia" && budget === "$" && grid === "offgrid" && kids === "no") {
-    result = "summer europe";
+    result = "winter Asia $ off grid no";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Asia" && budget === "$" && grid === "offgrid" && kids === "yes") {
-    result = "summer europe";
+    result = "winter Asia $ off grid yes";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Asia" && budget === "$" && grid === "resort" && kids === "no") {
-    result = "summer europe";
+    result = "winter Asia $ resort no";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Asia" && budget === "$" && grid === "resort" && kids === "yes") {
-    result = "summer europe";
+    result = "winter Asia $ resort yes";
     $("#recomendation").empty().text(result);
     $(".results").show();
   // winter europe
   } else if (season === "winter" && region === "Europe" && budget === "$$$$" && grid === "offgrid" && kids === "no") {
-    result = "summer europe";
+    result = "winter Europe $$$$ offgrid no";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Europe" && budget === "$$$$" && grid === "offgrid" && kids === "yes") {
-    result = "summer europe";
+    result = "winter Europe $$$$ offgrid yes";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Europe" && budget === "$$$$" && grid === "resort" && kids === "no") {
-  result = "summer europe";
+  result = "winter Europe $$$$ resort no";
   $("#recomendation").empty().text(result);
   $(".results").show();
   } else if (season === "winter" && region === "Europe" && budget === "$$$$" && grid === "resort" && kids === "yes") {
-    result = "summer europe";
+    result = "winter Europe $$$$ resort yes";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Europe" && budget === "$" && grid === "offgrid" && kids === "no") {
-    result = "summer europe";
+    result = "winter Europe";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Europe" && budget === "$" && grid === "offgrid" && kids === "yes") {
-    result = "summer europe";
+    result = "winter Europe";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Europe" && budget === "$" && grid === "resort" && kids === "no") {
-    result = "summer europe";
+    result = "winter Europe";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Europe" && budget === "$" && grid === "resort" && kids === "yes") {
-    result = "summer europe";
+    result = "winter Europe";
     $("#recomendation").empty().text(result);
     $(".results").show();
   // winter Americas
   } else if (season === "winter" && region === "Americas" && budget === "$$$$" && grid === "offgrid" && kids === "no") {
-    result = "summer americas";
+    result = "winter Americas";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Americas" && budget === "$$$$" && grid === "offgrid" && kids === "yes") {
-    result = "summer americas";
+    result = "winter Americas";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Americas" && budget === "$$$$" && grid === "resort" && kids === "no") {
-    result = "summer americas";
+    result = "winter Americas";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Americas" && budget === "$$$$" && grid === "resort" && kids === "yes") {
-    result = "summer americas";
+    result = "winter Americas";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Americas" && budget === "$" && grid === "offgrid" && kids === "no") {
-    result = "summer americas";
+    result = "winter Americas";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Americas" && budget === "$" && grid === "offgrid" && kids === "yes") {
-    result = "summer americas";
+    result = "winter Americas";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Americas" && budget === "$" && grid === "resort" && kids === "no") {
-    result = "summer americas";
+    result = "winter Americas";
     $("#recomendation").empty().text(result);
     $(".results").show();
   } else if (season === "winter" && region === "Americas" && budget === "$" && grid === "resort" && kids === "yes") {
-    result = "summer americas";
+    result = "winter Americas";
     $("#recomendation").empty().text(result);
     $(".results").show();
     }else{
