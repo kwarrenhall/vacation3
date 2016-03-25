47431050eda5bd7c38909ad88f0985717176ce44
kate Hall <kwarrenhall@gmail.com>
2016-03-25 10:04:51 -0700

    working js for four options

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   $("form#vacation").submit(function(event) {
 
-   var party = PartseInt($('#number').val());
    var season =$('#season').val();
    var region =$('#region').val();
    var budget =$('#budget').val();
+   var grid =$('#grid').val();
 
    if (season === "spring" && region === "North America" && budget === "expensive"
-) {
+&& grid === "offgrid") {
      var result = "rock"
     $("#recomendation").empty().text(result);
     $(".results").show();
